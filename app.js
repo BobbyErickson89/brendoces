a62df155ed89cb6628539105be1c9125ab186b0f
Bobby Erickson <ericksonr07@gmail.com>
2018-12-23 01:52:23 -0500

    Setting up views
    - Set up our views in app.js
    - Installed pug
    - Created initial welcome page
    - Connected our welcome page to our '/' route

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const routes = require('./routes/index');
 const port = 3000;
+
+// creating our Express app
+const app = express();
+
+// view engine setup.  Telling our app to look under views and use pug for our view engine.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // Allowing us to handle our routes
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Brendoces website');
+    res.render('welcome');
 });
 
 router.get('/menu', (req, res) => {
